208540adc80e5fac22228b607743107de601dda5
Gabriel Barsi Haberfeld <gbh2@illinois.edu>
2018-12-17 22:04:42 -0600

    Update grip.py

--- grip.py
+++ grip.py
@@ -1,6 +1,7 @@
 import sys
 #sys.path.insert(0, 'C:/Users/Tor/Desktop/Programming/Packages/Maestro-master')
 import maestro
+import time
 servo = maestro.Controller()
 servo.setAccel(0,0)
 servo.setAccel(1,0)
@@ -22,3 +23,7 @@ servo.setSpeed(1,0)
 
 servo.setTarget(0,1000)
 servo.setTarget(1,1000)
+time.sleep(2)
+servo.setTarget(0,7700)
+servo.setTarget(1,8100)
+
